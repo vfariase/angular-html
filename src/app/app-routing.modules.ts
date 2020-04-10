032e8abc52a2a47b1b1e730e42d9c3b9b2d9a271
vfarias <e>
2020-04-10 12:36:35 -0400

    Fin de la sección 3

---
+++ src/app/app-routing.modules.ts
@@ -0,0 +1,23 @@
+import { ItemComponent } from './pages/item/item.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { NgModule } from '@angular/core';
+import {Routes,RouterModule} from '@angular/router';
+
+
+const app_routes:Routes=[
+  {path:'home',component:PortafolioComponent},
+  {path:'about',component:AboutComponent},
+  {path:'item',component:ItemComponent},
+  {path:'**',pathMatch:'full',redirectTo:'home'}
+];
+
+@NgModule({
+imports: [RouterModule.forRoot(app_routes,{useHash:true})],
+
+exports:[RouterModule]
+})
+
+export class AppRoutingModule{
+
+}
